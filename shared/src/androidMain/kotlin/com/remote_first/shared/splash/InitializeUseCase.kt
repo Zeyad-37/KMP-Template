f391ba52d297f7e49916490fe871f0eab6d79aa5
Zeyad-37 <zeyad.gasser@glovoapp.com>
2020-12-06 12:15:45 +0100

    Share splash logic

---
+++ shared/src/androidMain/kotlin/com/remote_first/shared/splash/InitializeUseCase.kt
@@ -0,0 +1,28 @@
+package com.remote_first.shared.splash
+
+import android.Manifest.permission.ACCESS_COARSE_LOCATION
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.Context
+import androidx.core.content.PermissionChecker
+import androidx.core.content.PermissionChecker.checkSelfPermission
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+@FlowPreview
+@ExperimentalCoroutinesApi
+actual class InitializeUseCase @Inject constructor(@ApplicationContext val context: Context) {
+    actual fun execute(): Flow<SplashOutput> {
+        return flow {
+            if (checkSelfPermission(context, ACCESS_FINE_LOCATION) == PermissionChecker.PERMISSION_GRANTED
+                    && checkSelfPermission(context, ACCESS_COARSE_LOCATION) == PermissionChecker.PERMISSION_GRANTED) {
+                emit(NavToMain)
+            } else {
+                emit(RequestPermissions)
+            }
+        }
+    }
+}
--- splash/src/main/java/com/remote_first/splash/SplashInputHandler.kt
+++ splash/src/main/java/com/remote_first/splash/SplashInputHandler.kt
@@ -1,21 +1,21 @@
 package com.remote_first.splash
 
-import android.Manifest.permission.ACCESS_COARSE_LOCATION
-import android.Manifest.permission.ACCESS_FINE_LOCATION
 import com.remote_first.shared.flow_redux.InputHandler
 import com.remote_first.shared.flow_redux.RxOutcome
 import com.remote_first.shared.flow_redux.toEffectOutcome
 import com.remote_first.shared.flow_redux.toResultOutcome
+import com.remote_first.shared.splash.InitializeUseCase
+import com.remote_first.shared.splash.NavToMain
+import com.remote_first.shared.splash.RequestPermissions
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 @FlowPreview
 @ExperimentalCoroutinesApi
-class SplashInputHandler @Inject constructor(private val permissionChecker: PermissionChecker) :
+class SplashInputHandler @Inject constructor(private val initializeUseCase: InitializeUseCase) :
         InputHandler<SplashInput, SplashState> {
     override fun handleInputs(input: SplashInput, state: SplashState): Flow<RxOutcome> {
         return when (input) {
@@ -23,15 +23,10 @@ class SplashInputHandler @Inject constructor(private val permissionChecker: Perm
         }
     }
 
-    private fun onInitialize(): Flow<RxOutcome> {
-        return flow {
-            if (permissionChecker.hasPermission(ACCESS_FINE_LOCATION) &&
-                    permissionChecker.hasPermission(ACCESS_COARSE_LOCATION)) {
-                delay(1000)
-                emit(ToMain.toEffectOutcome())
-            } else {
-                emit(RequestPermissionsResult.toResultOutcome())
-            }
+    private fun onInitialize() = initializeUseCase.execute().map {
+        when (it) {
+            is NavToMain -> ToMain.toEffectOutcome()
+            is RequestPermissions -> RequestPermissionsResult.toResultOutcome()
         }
     }
 }
