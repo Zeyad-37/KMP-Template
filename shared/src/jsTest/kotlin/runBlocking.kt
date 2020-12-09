1b9491ff05a1e5d5cac1935f7d3613421313c329
Zeyad-37 <zeyad.gasser@glovoapp.com>
2020-12-09 11:41:30 +0100

    fix runBlocking.kt

--- shared/src/jsTest/kotlin/runBlocking.kt
+++ shared/src/jsTest/kotlin/runBlocking.kt
@@ -5,5 +5,5 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.promise
 import kotlin.coroutines.CoroutineContext
 
-actual fun <T> runBlocking(context: CoroutineContext, block: suspend CoroutineScope.() -> T): dynamic =
+fun <T> runBlocking(context: CoroutineContext, block: suspend CoroutineScope.() -> T): dynamic =
     GlobalScope.promise(context) { block() }
